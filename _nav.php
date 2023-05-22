8f7f7e4714695a624197e6d8202101ae28ca6114
MartinCharpentier92 <charpmartin@gmail.com>
2023-05-22 18:31:21 +0200

    Added index.php, _header.php and _nav.php

---
+++ _nav.php
@@ -0,0 +1,15 @@
+<nav id="nav">
+    <ul>
+        <?php if (!isset($_SESSION['user'])) { ?>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="register.php">Créer un compte</a></li>
+            <li><a href="login.php">Connexion</a></li>
+        <?php } else { ?>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="persos.php">
+                <?php echo $_SESSION['user']['nickname']; ?>
+            </a></li>
+            <li><a href="logout.php">Déconnexion</a></li>
+        <?php } ?>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require_once ('_header.php'); ?>
+
+    <h1>Bienvenue aventurier !</h1>
+
+</body>
+</html>
